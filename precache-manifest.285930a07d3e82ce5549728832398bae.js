78f7a8a9e390f60292443edc5340024ae8c03b77
Daniel <danielbuterus@gmail.com>
2020-05-22 04:08:17 +0600

    Updates

---
+++ precache-manifest.285930a07d3e82ce5549728832398bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318f421c784b60e4be878955e2ec6e83",
+    "url": "/coursework/index.html"
+  },
+  {
+    "revision": "4498af017975b2f2ff37",
+    "url": "/coursework/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f9690488c49c885cdf0b",
+    "url": "/coursework/static/js/2.0734857d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coursework/static/js/2.0734857d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4498af017975b2f2ff37",
+    "url": "/coursework/static/js/main.93b88bcd.chunk.js"
+  },
+  {
+    "revision": "22a02a2fef555b4233c0",
+    "url": "/coursework/static/js/runtime-main.e6464175.js"
+  }
+]);
