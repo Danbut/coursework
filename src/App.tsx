205085ce9fb4ccd6101cb5722d2ec480b91aa509
Daniel <danielbuterus@gmail.com>
2020-05-22 15:54:24 +0600

    feat: moved code from another repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,209 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import clsx from 'clsx';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import { Toolbar, IconButton, Typography, AppBar, CssBaseline, Drawer, Divider, List, Button } from '@material-ui/core';
+import {
+    Menu as MenuIcon,
+    ChevronLeft as ChevronLeftIcon,
+    ChevronRight as ChevronRightIcon,
+} from '@material-ui/icons/';
+import AceEditor from 'react-ace';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/theme-github';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            height: '100vh',
+        },
+        appBar: {
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        hide: {
+            display: 'none',
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        drawerHeader: {
+            display: 'flex',
+            alignItems: 'center',
+            padding: theme.spacing(0, 1),
+            ...theme.mixins.toolbar,
+            justifyContent: 'space-between',
+        },
+        content: {
+            flexGrow: 1,
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            marginLeft: -drawerWidth,
+        },
+        contentShift: {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        },
+        editors: {
+            width: '100%',
+            height: `calc(100vh - 160px)`,
+            display: 'flex',
+            flex: '0 0 auto',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            [theme.breakpoints.between('xs', 'md')]: {
+                flexDirection: 'column',
+            },
+        },
+    }),
+);
+
+const App: React.FC = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(true);
+
+    const handleDrawerOpen: () => void = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose: () => void = () => {
+        setOpen(false);
+    };
+
+    const onChange: (newValue: string) => void = (newValue: string) => {
+        console.log('change', newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h5" noWrap>
+                        Form Editor
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <Typography variant="h5" noWrap>
+                        Commands
+                    </Typography>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List></List>
+                <Divider />
+                <List></List>
+            </Drawer>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+                style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}
+            >
+                <div className={classes.drawerHeader} />
+                <div className={classes.editors}>
+                    <div
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-evenly',
+                        }}
+                    >
+                        <Typography variant="h6" noWrap style={{ width: '50%', paddingLeft: '40px' }}>
+                            Command:
+                        </Typography>
+                        <AceEditor
+                            mode="json"
+                            theme="github"
+                            onChange={onChange}
+                            name="UNIQUE_ID_OF_DIV"
+                            editorProps={{ $blockScrolling: true }}
+                            style={{ width: '100%', height: '90%' }}
+                        />
+                    </div>
+                    <div
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-evenly',
+                        }}
+                    >
+                        <Typography variant="h6" noWrap style={{ width: '50%', paddingLeft: '40px' }}>
+                            Form:
+                        </Typography>
+                        <AceEditor
+                            mode="json"
+                            theme="github"
+                            onChange={onChange}
+                            name="UNIQUE_ID_OF_DIV"
+                            editorProps={{ $blockScrolling: true }}
+                            style={{ width: '100%', height: '90%' }}
+                            readOnly={true}
+                        />
+                    </div>
+                </div>
+                <Button variant="contained" color="primary" style={{ margin: 'auto', width: '55%' }}>
+                    Execute
+                </Button>
+            </main>
+        </div>
+    );
+};
 
 export default App;
